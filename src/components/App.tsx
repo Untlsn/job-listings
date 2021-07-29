230819729611a5c60edeec80eab2608dcfbfc2ff
Untel <Untellson@gmail.com>
2021-07-29 13:35:15 +0200

    Build pass

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,8 +3,6 @@ import { GlobalStyle } from '~/assets/style/global';
 import Main from '~/components/view/Main';
 import { Provider as OfferDataProvider } from '~/data/OfferData';
 import { Provider as FilterProvider } from '~/data/Filters';
-import { enableMapSet } from 'immer';
-enableMapSet();
 
 const App = () => {
   return (
